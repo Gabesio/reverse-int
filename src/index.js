ae7b5e8165361b9ec06c0582c94e836016209e8e
Georgi Strelkov <georg.strelkov@mail.ru>
2023-07-25 12:16:25 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
+    let i = 0;
+    let result = '';
+    let num = n.toString();
+
+    while (i < num.length) {
+        if (num[0] !== '-') {
+            result = num[i] + result;
+            i = i + 1;
+        } else {
+            num = num.slice(1);
+            result = num[i] + result;
+            i = i + 1;
+        }
+    }
+    return result;
 }
